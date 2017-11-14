a23dbbd2cd71c9f58a049a17845845aabd4c859d
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-11-14 22:41:36 +0100

    use users route

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from routes.entry import entry
+from routes.users import users
 
 app = Flask(__name__)
 
 app.register_blueprint(entry)
+app.register_blueprint(users)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=80)
