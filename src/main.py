1a057bb839f422e3936ef3859b02f86c3876bf2c
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-11-14 19:50:03 +0100

    default main

--- src/main.py
+++ src/main.py
@@ -1,31 +1,9 @@
-import os
-from flask import Flask, redirect, url_for, request, render_template
-from pymongo import MongoClient
+from flask import Flask
+from routes.entry import entry
 
 app = Flask(__name__)
 
-client = MongoClient('mongodb:27017')
-db = client.tododb
-
-@app.route('/')
-def todo():
-
-    _items = db.tododb.find()
-    items = [item for item in _items]
-
-    return render_template('todo.html', items=items)
-
-
-@app.route('/new', methods=['POST'])
-def new():
-
-    item_doc = {
-        'name': request.form['name'],
-        'description': request.form['description']
-    }
-    db.tododb.insert_one(item_doc)
-
-    return redirect(url_for('todo'))
+app.register_blueprint(entry)
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='0.0.0.0', debug=True, port=80)
