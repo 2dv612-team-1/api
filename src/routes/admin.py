6b02d0feb757021cbb85e5adf463dbdfa4ce8793
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-11-15 13:24:35 +0100

    Can login as admin

--- src/routes/admin.py
+++ src/routes/admin.py
@@ -4,6 +4,7 @@ Admin routes
 
 from flask import Blueprint, request, jsonify
 from pymongo import MongoClient
+import jwt
 
 client = MongoClient('mongodb:27017')
 admin = Blueprint('admin', __name__)
@@ -23,4 +24,29 @@ def adminActions():
         return jsonify({
             'status': 201,
             'message': 'Admin account has been created'
-        })
+        }), 201
+
+@admin.route('/admins/auth', methods=['POST'])
+def adminAuth():
+    if request.method == 'POST':
+        try:
+            username = request.form['username']
+            password = request.form['password']
+
+            foundAdmin = db.admin.find_one({'username': username, 'password': password})
+
+            if foundAdmin:
+                payload = {'username': username, 'role': 'admin'}
+                encoded = jwt.encode(payload, 'super-secret')
+                return jsonify({
+                    'token': encoded.decode('utf-8'),
+                    'message': 'Successfully logged in as admin',
+                    'status': 200
+                }), 200
+            else:
+                raise AttributeError()
+        except AttributeError:
+            return jsonify({
+                'message': 'Wrong credentials',
+                'status': 400
+            }), 400
