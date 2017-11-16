bb44b2c61921cff1ef96bab2adcf68a882c5e89c
slaxku <Rickardthom@outlook.com>
2017-11-16 11:02:30 +0100

    initPush

--- src/routes/entry.py
+++ src/routes/entry.py
@@ -5,6 +5,7 @@ entry = Blueprint('entry', __name__)
 def getEntry():
   routes = {
     'representatives': '/representatives',
+    'companies': '/companies',
     'admins': '/admins',
     'self': '/'
   }
