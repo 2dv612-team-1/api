495b70747789b9b3ec46c2c653a4b614174ac84d
slaxku <Rickardthom@outlook.com>
2017-11-20 20:01:01 +0100

    Added company data and company test

--- src/tests/test_create_users.py
+++ src/tests/test_create_users.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 import unittest
 import json
 
+"""Random tests writing and reading """
 class UsersTest(unittest.TestCase):
 
     def setUp(self):
@@ -17,36 +18,46 @@ class UsersTest(unittest.TestCase):
     def tearDown(self):
         self.__deleteTestDataInDB()
     
-    def test_checkForRole(self):
+    def test_runTestDepOnRole(self):
         for user in self.__db_users.find():
             if user['role'] == 'admin':
-                self.__testinsertedAdminData(user)
+                self.__testInsertedAdminData(user)
             elif user['role'] == 'consumer':
-                self.__testinsertedConsumerData(user)
+                self.__testInsertedConsumerData(user)
+            elif user['role'] == 'company':
+                self.__testInsertedCompanyData(user)
             else:
                 pass
 
-    def __testinsertedConsumerData(self, user):
+    def __testInsertedConsumerData(self, user):
         self.assertEqual(user['password'], 'consumer1')
         self.assertEqual(len(user['data']['Manuals']), 3)
 
-    def __testinsertedAdminData(self, user):
+    def __testInsertedAdminData(self, user):
         self.assertEqual(user['password'], '1234')
         self.assertEqual(user['data']['Dell']['username'], 'userDell')
-
+    
+    def __testInsertedCompanyData(self, user):
+        self.assertEqual(user['password'], 'passDell')
+        self.assertEqual(len(user['data']['Representatives']), 3)
+        
     def __createTestData(self):
         """" Consumer Data """
         consumer_data = {'PersonalInfo':{'name' : 'Eggbert', 'age' : 34},
                         'Manuals': ('manual_1', 'manual_2', 'manual_2')}
-
-
         self.__test_data.append({'username' : 'consumer1', 'password' : 'consumer1', 'role' : 'consumer', 'data' : consumer_data})
 
         """ Admin Data """
         admin_data = {'Dell' :{ 'username' : 'userDell', 'password' : 'passDell'},
                         'Apple':{'username' : 'userApple', 'password': 'passApple'}}
-
         self.__test_data.append({'username' : 'admin', 'password' : '1234', 'role' : 'admin', 'data' : admin_data})
+
+        """ Company Data """
+        company_data = {'Representatives' : [{'username' : 'rep1', 'password' : 'rep1'},
+                                            {'username' : 'rep2', 'password' : 'rep2'},
+                                            {'username' : 'rep3', 'password' : 'rep3'}]}
+
+        self.__test_data.append({'username' : 'userDell', 'password' : 'passDell', 'role' : 'company', 'data' : company_data})
 
     def __saveTestDataToDB(self):
         for user in self.__test_data:
