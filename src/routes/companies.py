9f81a44ad092de9b3d95da3485e4fdfe4cffa88a
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-11-21 23:47:29 +0100

    Gets all representatives

--- src/routes/companies.py
+++ src/routes/companies.py
@@ -14,45 +14,59 @@ DB = CLIENT.api
 # add bcrypt
 
 
-@COMPANIES.route('/companies', methods=['GET', 'POST'])
+@COMPANIES.route('/companies')
 def company_actions():
-
     """Extracts companies"""
-    if request.method == 'GET':
+    try:
+        data = []
+        for company in DB.users.find({'role': 'company'}):
+            data.append({'username': company['username']})
 
-        try:
-            data = []
-            for company in DB.users.find({'role': 'company'}):
-                data.append({'username': company['username']})
+        return response('Successfully extracted all companies', 200, {'companies': data})
+    except SystemError:
+        return response('Something went wrong while retreiving the data', 500)
 
-            return response('Successfully extracted all companies', 200, {'companies': data})
-        except SystemError:
-            return response('Something went wrong while retreiving the data', 500)
 
+@COMPANIES.route('/companies', methods=['POST'])
+def company_creation():
     """Creates company"""
-    if request.method == 'POST':
+    try:
         form = request.form
+        token = form['jwt']
+        payload = jwt.decode(token, 'super-secret')
 
-        try:
-            token = form['jwt']
-            payload = jwt.decode(token, 'super-secret')
+        if payload['role'] == 'admin':
+            username = form['username']
+            password = form['password']
 
-            if payload['role'] == 'admin':
-                username = form['username']
-                password = form['password']
+            company = {
+                'username': username,
+                'password': password,
+                'role': 'company'
+            }
 
-                company = {
-                    'username': username,
-                    'password': password,
-                    'role': 'company'
-                }
+            company_exists = DB.users.find_one({'username': username})
 
-                company_exists = DB.users.find_one({'username': username})
+            if company_exists:
+                return response('Company already exists', 409)
+            else:
+                DB.users.insert(company)
+                return response('Company was created', 201)
+        else:
+            return response('You have to be an admin to create company', 400)
+    except AttributeError:
+        return response('Wrong credentials', 400)
 
-                if company_exists:
-                    return response('Company already exists', 409)
-                else:
-                    DB.users.insert(company)
-                    return response('Company was created', 201)
-        except AttributeError:
-            return response('Wrong credentials', 400)
+
+@COMPANIES.route('/companies/<name>/representatives')
+def get_representatives(name):
+    """Gets list of representatives from specific company"""
+
+    company = DB.users.find_one({'username': name})
+    if company:
+        representatives = []
+        for representative in DB.users.find({'owner': name}):
+            representatives.append({'username': representative['username']})
+        return response(name, 200, {'representatives': representatives})
+    else:
+        return response('Invalid company', 400)
