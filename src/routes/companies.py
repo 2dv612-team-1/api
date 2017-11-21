7da9d3ce603fb79c4e44768bf0ed31ea85da2dcd
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-11-22 00:02:17 +0100

    Can create representatives

--- src/routes/companies.py
+++ src/routes/companies.py
@@ -61,7 +61,6 @@ def company_creation():
 @COMPANIES.route('/companies/<name>/representatives')
 def get_representatives(name):
     """Gets list of representatives from specific company"""
-
     company = DB.users.find_one({'username': name})
     if company:
         representatives = []
@@ -70,3 +69,35 @@ def get_representatives(name):
         return response(name, 200, {'representatives': representatives})
     else:
         return response('Invalid company', 400)
+
+
+@COMPANIES.route('/companies/<name>/representatives', methods=['POST'])
+def create_representative(name):
+    """Creates representative"""
+    try:
+        form = request.form
+        token = form['jwt']
+        payload = jwt.decode(token, 'super-secret')
+
+        if payload['role'] == 'company':
+            username = form['username']
+            password = form['password']
+
+            representative = {
+                'username': username,
+                'password': password,
+                'owner': name,
+                'role': 'representative'
+            }
+
+            representative_exists = DB.users.find_one({'username': username})
+
+            if representative_exists:
+                return response('Username already exists', 409)
+            else:
+                DB.users.insert(representative)
+                return response('Representative was created', 201)
+        else:
+            return response('You are not a company', 400)
+    except AttributeError:
+        return response('Wrong credentials', 400)
