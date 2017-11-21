2399b19f711d7fc28db1c0c67d13de8d4c7f7847
Pär Eriksson <pe222gq@student.lnu.se>
2017-11-21 14:45:19 +0100

    Adding CRUD skeleton.

--- src/dao/DAO.py
+++ src/dao/DAO.py
@@ -14,9 +14,23 @@ class DAO:
     self.DB = self.CLIENT.api
 
   def get(self, collection_name):
+    """Get all"""
+
     result = []
     collection = self.DB[collection_name]
     items = collection.find()
     for item in items:
       result.append({'username': item['username']})
     return result
+
+  def create(self, collection_name, item):
+    """Creates"""
+
+  def get_by_id(self, collection_name, item):
+    """Get one"""
+
+  def update(self, collection_name, item, item_id):
+    """Updates"""
+
+  def delete(self, collection_name, item_id):
+    """Deletes"""
