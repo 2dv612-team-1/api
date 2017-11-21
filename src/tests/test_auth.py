b017cb2a1739476f94e7a7e8466a7397e8b9328b
slaxku <Rickardthom@outlook.com>
2017-11-21 21:38:44 +0100

    fix

--- src/tests/test_auth.py
+++ src/tests/test_auth.py
@@ -19,9 +19,12 @@ class AuthTestCase(unittest.TestCase):
         self.__db_helper.deleteTestDataInDB()
 
     def test_authAsAdmin(self):
+        username = 'admin'
+        password = '1234'
+
         response_auth = self.__app.post('/auth',
-                                        data=dict({'username': 'admin', 'password': '1234'}))
+                                        data=dict({'username': username, 'password': password}))
         auth_data = json.loads(response_auth.data)
         self.assertEqual(response_auth.status_code, 200)
-        self.assertEqual(auth_data['message'], 'Successfully logged in as ' + self.__db_helper.getRoleForUser('admin')['role'])
+        self.assertEqual(auth_data['message'], 'Successfully logged in as ' + self.__db_helper.getRoleForUser(username)['role'])
         self.assertTrue(auth_data['token'])
