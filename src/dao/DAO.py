3b630bac3340145f61a06597a161f9ee2ecd9bea
Pär Eriksson <pe222gq@student.lnu.se>
2017-11-21 14:48:13 +0100

    Linting cleanup.

--- src/dao/DAO.py
+++ src/dao/DAO.py
@@ -4,33 +4,32 @@ Data Access Layer/Object
 
 from pymongo import MongoClient
 
-# CLIENT = MongoClient('mongodb:27017')
-# DB = CLIENT.api
 
 class DAO:
 
-  def __init__(self):
-    self.CLIENT = MongoClient('mongodb:27017')
-    self.DB = self.CLIENT.api
+    def __init__(self):
+        self.CLIENT = MongoClient('mongodb:27017')
+        self.DB = self.CLIENT.api
 
-  def get(self, collection_name):
-    """Get all"""
+    def get(self, collection_name):
+        """Get all"""
 
-    result = []
-    collection = self.DB[collection_name]
-    items = collection.find()
-    for item in items:
-      result.append({'username': item['username']})
-    return result
+        result = []
+        collection = self.DB[collection_name]
+        items = collection.find()
 
-  def create(self, collection_name, item):
-    """Creates"""
+        for item in items:
+            result.append({'username': item['username']})
+        return result
 
-  def get_by_id(self, collection_name, item):
-    """Get one"""
+    def create(self, collection_name, item):
+        """Creates"""
 
-  def update(self, collection_name, item, item_id):
-    """Updates"""
+    def get_by_id(self, collection_name, item):
+        """Get one"""
 
-  def delete(self, collection_name, item_id):
-    """Deletes"""
+    def update(self, collection_name, item, item_id):
+        """Updates"""
+
+    def delete(self, collection_name, item_id):
+        """Deletes"""
