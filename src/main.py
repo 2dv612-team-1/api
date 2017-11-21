42175c5e9f1a7cf3e7fde0c8a75c82965d64afc1
beppek <beppe@live.se>
2017-11-21 15:20:20 +0100

    adds rate limit 10/hr or 50/day

--- src/main.py
+++ src/main.py
@@ -8,9 +8,16 @@ from routes.representatives import REPRESENTATIVES
 from routes.admin import ADMIN
 from routes.companies import COMPANIES
 from flask_cors import CORS
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 APP = Flask(__name__)
 CORS(APP)
+LIMITER = Limiter(
+    APP,
+    key_func=get_remote_address,
+    default_limits=["50 per day", "10 per hour"]
+)
 
 APP.register_blueprint(ENTRY)
 APP.register_blueprint(REPRESENTATIVES)
