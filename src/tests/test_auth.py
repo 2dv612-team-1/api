e011542fd60ab8985f88d0c304c92ce858141165
slaxku <Rickardthom@outlook.com>
2017-11-22 12:05:33 +0100

    refactoring and added test for auth with invalid username and password

--- src/tests/test_auth.py
+++ src/tests/test_auth.py
@@ -7,15 +7,20 @@ class AuthTestCase(BaseTest):
 
     def test_authEveryUserInDB(self):
         for user in self._db_helper.getUsers():
-            self.__authUser(user)
+            self.__authWithValidUserCredentials(user['username'], user['password'])
 
-    def __authUser(self, user):
-        username = user['username']
-        password = user['password']
+    def test_authInvalidUsernameAndPassword(self):
+        auth_data = self.__getResponseDataFromPostRequest('badusername', 'badpassword')
+        self.assertEquals(auth_data['status'], 400)
+        self.assertEqual(auth_data['message'],'Wrong credentials')
 
-        response_auth = self._app.post('/auth', data=dict({'username': username, 'password': password}))
-        auth_data = json.loads(response_auth.data)
-
-        self.assertEqual(response_auth.status_code, 200)
-        self.assertEqual(auth_data['message'], 'Successfully logged in as ' + self._db_helper.getRoleForUser(username)['role'])
+    def __authWithValidUserCredentials(self, username, password):
+        auth_data = self.__getResponseDataFromPostRequest(username, password)
+        self.assertEqual(auth_data['status'], 200)
+        self.assertEqual(auth_data['message'],
+                             'Successfully logged in as ' + self._db_helper.getRoleForUser(username)['role'])
         self.assertTrue(auth_data['token'])
+
+    def __getResponseDataFromPostRequest(self, username, password):
+        response_auth = self._app.post('/auth', data=dict({'username': username, 'password': password}))
+        return json.loads(response_auth.data)
