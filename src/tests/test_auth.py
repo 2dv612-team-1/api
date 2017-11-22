60450e02c39420b27584b7ed2b325fde7406fc51
slaxku <Rickardthom@outlook.com>
2017-11-22 11:49:29 +0100

    updated auth to test auth on every user in db

--- src/tests/test_auth.py
+++ src/tests/test_auth.py
@@ -5,9 +5,13 @@ from basetest import BaseTest
 
 class AuthTestCase(BaseTest):
 
-    def test_authAsAdmin(self):
-        username = 'admin'
-        password = '1234'
+    def test_authEveryUserInDB(self):
+        for user in self._db_helper.getUsers():
+            self.__authUser(user)
+
+    def __authUser(self, user):
+        username = user['username']
+        password = user['password']
 
         response_auth = self._app.post('/auth', data=dict({'username': username, 'password': password}))
         auth_data = json.loads(response_auth.data)
