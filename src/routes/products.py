9dd1125d27b4ce1bbdfea89e62274a12a5b8175d
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-11-28 23:22:46 +0100

    Creates product

--- src/routes/products.py
+++ src/routes/products.py
@@ -16,6 +16,7 @@ PRODUCTS = Blueprint('products', __name__)
 CLIENT = MongoClient('mongodb:27017')
 DB = CLIENT.api
 
+
 @PRODUCTS.route('/products')
 def get_products():
     """Gets all available products"""
@@ -33,6 +34,7 @@ def get_products():
 
     return response(products_data, 200)
 
+
 @PRODUCTS.route('/products', methods=['POST'])
 def create_product():
     """Create a product"""
@@ -47,12 +49,21 @@ def create_product():
     except Exception:
         return response('Tampered token', 400)
 
-    try:
-        category = request.form['category']
-        title = request.form['title']
-        description = request.form['description']
-    except Exception:
-        return response('Wrong information', 400)
+    if payload['role'] == 'representative':
+        try:
+            new_product = {
+                'category': request.form['category'],
+                'title': request.form['title'],
+                'description': request.form['description']
+            }
+        except Exception:
+            return response('Wrong information', 400)
+
+        if DB.products.find(new_product):
+            return response('Product already exists', 409)
+
+        DB.products.insert(new_product)
+        return response('Product was created', 201)
 
 
 @PRODUCTS.route('/products/upload', methods=['POST'])
