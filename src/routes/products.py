a832291b58621958a1fa967df5ab5f4846011de3
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-11-29 13:35:26 +0100

    Removed unused route

--- src/routes/products.py
+++ src/routes/products.py
@@ -119,23 +119,3 @@ def get_product(_id):
         return response(get_product, 200)
     else:
         return response('No product with that id', 200)
-
-
-@PRODUCTS.route('/products/upload', methods=['POST'])
-def upload_actions():
-    if 'file' not in request.files:
-        return response('File param should be \'file\'', 400)
-    file = request.files['file']
-    file_category = request.form['category']
-    # if user does not select file, browser also
-    # submit a empty part without filename
-    if file.filename == '':
-        return response('No file present in request', 400)
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file_folder = os.path.join(UPLOAD_FOLDER, file_category)
-        if not os.path.exists(file_folder):
-            os.makedirs(file_folder)
-        file_path = os.path.join(file_folder, filename)
-        file.save(file_path)
-        return response('Successfully uploaded the file', 200)
