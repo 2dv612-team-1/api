58155d74fa2361e1b370aa6ecdbf715348c27e4e
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-11-30 00:11:57 +0100

    If no file found, 404

--- src/routes/materials.py
+++ src/routes/materials.py
@@ -3,6 +3,7 @@ Materials route
 """
 
 from flask import Blueprint, send_from_directory
+from utils.response import response
 from pymongo import MongoClient
 
 UPLOAD_FOLDER = './uploads'
@@ -13,4 +14,7 @@ DB = CLIENT.api
 
 @MATERIALS.route('/materials/<company>/<path:filename>')
 def get_file(company, filename):
-    return send_from_directory(UPLOAD_FOLDER + '/' + company, filename)
+    try:
+        return send_from_directory(UPLOAD_FOLDER + '/' + company, filename)
+    except Exception:
+         return response('No such file', 400)
