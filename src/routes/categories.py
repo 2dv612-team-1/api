cf8b2150822ba5184b6ef841f615cf46c933c068
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-12-06 15:05:53 +0100

    Returns status

--- src/routes/categories.py
+++ src/routes/categories.py
@@ -3,7 +3,7 @@ Categories Route
 """
 
 from flask import Blueprint, request
-from dal.categories import dal_get_categories, dal_create_category
+from dal.categories import dal_get_categories, dal_create_category, dal_create_subcategory
 from utils.response import response
 from exceptions.WrongCredentials import WrongCredentials
 from exceptions.AlreadyExists import AlreadyExists
@@ -23,10 +23,8 @@ def create_categories():
     """Creates a new category"""
 
     try:
-
         dal_create_category(request.form)
         return response('Category created', 201)
-
     except AttributeError:
         return response('Broken JWT', 400)
     except WrongCredentials:
@@ -35,12 +33,13 @@ def create_categories():
         return response('Category exists', 409)
 
 
-@CATEGORIES.route('/categories/<category>/subcategories')
-def create_sub():
+@CATEGORIES.route('/categories/<category>/subcategories', methods=['POST'])
+def create_sub(category):
     """Create a subcategory"""
 
     try:
-        
+        sub = dal_create_subcategory(request.form, category)
+        return response('Subcategory created', 201)
     except Exception:
-
-
+        return response('Could not create subcategort', 400)
+    
