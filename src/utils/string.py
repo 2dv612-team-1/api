2c0db35fc35cd1ea6a3175449fdaeae8faced924
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-12-11 14:43:16 +0100

    Added strings file

---
+++ src/utils/string.py
@@ -0,0 +1,40 @@
+## COMMON ##
+USERNAME = 'username'
+PASSWORD = 'password'
+ROLE = 'role'
+OWNER = 'owner'
+DATA = 'data'
+TOKEN = 'token'
+
+
+## ROLES ##
+ADMIN = 'admin'
+REPRESENTATIVE = 'representative'
+COMPANY = 'company'
+CONSUMER = 'consumer'
+
+## DATA ##
+CATEGORY = 'category'
+ID = '_id'
+SUB = 'sub'
+JWT = 'jwt'
+NAME = 'name'
+DESCRIPTION = 'description'
+SUBCATEGORY = 'subcategory'
+SERIALNO = 'serialNo'
+CREATEDBY = 'createdBy'
+PRODUCER = 'producer'
+COMPANIES = 'companies'
+USERS = 'users'
+
+## ROUTES ##
+AUTH = 'auth'
+PRODUCTS = 'products'
+CONSUMERS = 'consumers'
+COMPANIEsS = 'companies'
+ADMINS = 'admins'
+MATERIALS = 'materials'
+CATEGORIES = 'categories'
+ENTRY = 'entry'
+SELF = 'self'
+ROOT = '/'
