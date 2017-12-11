4b867c9ac4b17ef9427b743de6a62ce91bafd5e2
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-12-11 14:28:22 +0100

    Deps and uneccessary if statements

--- src/routes/admin.py
+++ src/routes/admin.py
@@ -4,6 +4,7 @@ Admin routes
 
 from flask import Blueprint, request
 from utils.response import response
+from utils.string import *
 from dal.admin import auth_and_return_admin, create_default_admin
 
 ADMIN = Blueprint('admin', __name__)
@@ -14,8 +15,7 @@ ADMIN = Blueprint('admin', __name__)
 def admin_actions():
     """When requested create admin account"""
 
-    if request.method == 'POST':
-        create_default_admin()
+    create_default_admin()
     return response('Admin account has been created', 201)
 
 
@@ -23,11 +23,9 @@ def admin_actions():
 def admin_auth():
     """Authenticates an admin"""
 
-    if request.method == 'POST':
-        try:
+    try:
+        encoded_data = auth_and_return_admin(request.form)
+        return response('Successfully logged in as admin', 200, {TOKEN: encoded_data.decode('utf-8')})
 
-            encoded_data = auth_and_return_admin(request.form)
-            return response('Successfully logged in as admin', 200, {'token': encoded_data.decode('utf-8')})
-
-        except AttributeError:
-            return response('Wrong credentials', 400)
+    except AttributeError:
+        return response('Wrong credentials', 400)
