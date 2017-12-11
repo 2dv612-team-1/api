c91a8cc47bef5eb0e2af87f6d77e62d22a61c892
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-12-11 14:43:07 +0100

    Unused code

--- src/routes/materials.py
+++ src/routes/materials.py
@@ -4,12 +4,11 @@ Materials route
 
 from flask import Blueprint, send_from_directory
 from utils.response import response
+from utils.string import *
 from pymongo import MongoClient
 
-UPLOAD_FOLDER = './materials'
 MATERIALS = Blueprint('materials', __name__)
-CLIENT = MongoClient('mongodb:27017')
-DB = CLIENT.api
+UPLOAD_FOLDER = './materials'
 
 
 @MATERIALS.route('/materials/<company>/<product_id>/<path:filename>')
@@ -17,4 +16,4 @@ def get_file(company, product_id, filename):
     try:
         return send_from_directory(UPLOAD_FOLDER + '/' + company + '/' + product_id, filename)
     except Exception:
-         return response('No such file', 404)
+        return response('No such file', 404)
