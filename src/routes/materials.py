24ff84e9071c63e394f232f49486d09200d3d4c5
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-12-12 19:43:07 +0100

    Uses material string dep

--- src/routes/materials.py
+++ src/routes/materials.py
@@ -7,11 +7,11 @@ from utils.response import response
 from utils.string import *
 from pymongo import MongoClient
 
-MATERIALS = Blueprint('materials', __name__)
+MATERIALS_ROUTER = Blueprint(MATERIALS, __name__)
 UPLOAD_FOLDER = './materials'
 
 
-@MATERIALS.route('/materials/<company>/<product_id>/<path:filename>')
+@MATERIALS_ROUTER.route('/materials/<company>/<product_id>/<path:filename>')
 def get_file(company, product_id, filename):
     try:
         return send_from_directory(UPLOAD_FOLDER + '/' + company + '/' + product_id, filename)
