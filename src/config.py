7e8b7ff503ecee1b342cf68a6518499910627cdf
beppek <beppe@live.se>
2017-12-14 08:20:55 +0100

    better secret ;)

--- src/config.py
+++ src/config.py
@@ -1,3 +1,3 @@
 # config things here
-SECRET='super-mega-secret-stuff-but-oscar-is-the-best'
+SECRET='super-mega-secret-stuff-but-oscar-is-the-best-but-not-really-because-beppe-is-better'
 MONGO='mongodb:27017'
