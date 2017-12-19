6b198f48ffc96d4dfd58b252a3a674ffa68d0ff0
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-12-19 20:22:15 +0100

    Can get single thread

--- src/dal/threads.py
+++ src/dal/threads.py
@@ -2,8 +2,9 @@ from .mongo_client import db_conn
 from utils.string import *
 from utils.jwt_handler import *
 from config import *
-
+from bson.objectid import ObjectId
 from exceptions.BadFormData import BadFormData
+from exceptions.NotFound import NotFound
 
 """
     db_conn.threads => client.api.threads
@@ -49,6 +50,20 @@ def dal_create_thread(form, payload):
         raise BadFormData('Could not create thread with supplied data')
 
     return str(_id)
+
+
+def dal_get_thread(id):
+    try:
+        thread = db_conn.threads.find_one({ID: ObjectId(id)})
+    except Exception:
+        raise NotFound('Could not find thread')
+
+    try:
+        thread_data = list(map(lambda key: [key, str(thread[key])], thread.keys()))
+    except Exception as e:
+        return str(e)
+
+    return dict(thread_data)
 
 
 def set(dict, form):
